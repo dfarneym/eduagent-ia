5fa7136c39b5800362ec7050e1b40344292478b7
farney <f_farney@hotmail.com>
2026-08-04 11:15:30 -0300

    chore: iniyalize project architecture

---
+++ app.py
@@ -0,0 +1,5 @@
+#"Vá até a interface e importe a função que inicia a aplicação."
+from eduagent.ui.main_page import run_app
+
+if __name__ == "__main__":
+    run_app()
---
+++ src/eduagent/config/settings.py
@@ -0,0 +1,32 @@
+"""
+Essa BaseSettings importação permite carregar automaticamente as configurações
+do arquivo .env.
+Já SettingsConfigDict informa ao Pydantic onde está o  .env
+e como ele deve ser lido
+"""
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+# Essa classe representa todas as configurações da aplicação
+class Settings(BaseSettings):
+    # LLM
+    GOOGLE_API_KEY: str = ""
+    MODEL_NAME: str = "gemini-2.5-flash"
+
+    # DiretóriosEvitamos de escrever vários arquivos
+    DOCUMENTS_PATH: str = "data/documents"
+    VECTORSTORE_PATH: str = "data/vectorstore"
+
+    # Aplicação
+    APP_NAME: str = "EduAgent AI"
+    APP_VERSION: str = "0.1.0"
+
+    #Lendo as variaveis de ambiente do arquivo .env
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    
+    )
+
+# Essa vai ser a instancia utilizada em toda a aplicação
+settings = Settings()
